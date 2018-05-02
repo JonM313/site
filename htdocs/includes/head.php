68d4b0ed952de2b245af632ffce9f243ae8477ab
Eridion Kiorai <erikiorai@gmail.com>
2018-05-02 18:48:29 +0300

    added htm 5 beta version

---
+++ htdocs/includes/head.php
@@ -0,0 +1,4 @@
+<title><?php echo $title; ?></title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="icon" href="../images/favicon.ico" type="image/x-icon"/>
+	<link rel="shortcut icon" href="../images/shorcut.ico" type="image/x-icon"/>
---
+++ htdocs/index.php
@@ -0,0 +1,4 @@
+<?php
+$title = "test";
+include ("include/layout.php");
+?>
